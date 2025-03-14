41dc31030189b02e422465dcc980a7b5be244c3e
muhammadkhalid-03 <muhammadkhalid200314@gmail.com>
2025-03-14 18:31:19 -0500

    my items feature w/ favorites bug

--- app/additionalinfo/MyItems.tsx
+++ app/additionalinfo/MyItems.tsx
@@ -1,10 +1,131 @@
-import { View, Text } from "react-native";
+import Categories from "@/components/Categories";
+import Header from "@/components/Header";
+import ProductList from "@/components/ProductList";
+import { CategoryType, ItemType } from "@/types/types";
+import { Stack } from "expo-router";
+import { useEffect, useState } from "react";
+import { View, Text, Alert } from "react-native";
+import { useFavorites } from "../contexts/FavoritesContext";
+import { useAuth } from "../contexts/AuthContext";
+import { useSearch } from "../contexts/SearchContext";
+import axios from "axios";
 
 const MyItems = () => {
+  const [items, setItems] = useState<ItemType[]>([]);
+  const [filteredItems, setFilteredItems] = useState<ItemType[]>([]);
+  const [categories, setCategories] = useState<CategoryType[]>([]);
+
+  const { authToken } = useAuth(); //auth context
+  const { favoriteIds, toggleFavorite, isFavorite } = useFavorites(); //favorites context
+
+  const {
+    searchQuery,
+    filteredResults,
+    isLoading,
+    toggleCategory,
+    clearCategories,
+    selectedCategory,
+    setSelectedCategory,
+  } = useSearch(); // search context
+
+  // Decide which items to show:
+  const itemsToDisplay = searchQuery !== "" ? filteredResults : filteredItems;
+
+  //this will run every time authToken changes
+  useEffect(() => {
+    if (authToken) {
+      console.log("Token has been updated:", authToken);
+    }
+  }, [authToken]); // dependency for authToken changes
+
+  //this will run ONLY once when the component initially mounts
+  useEffect(() => {
+    // getCategories();
+    getUserItems();
+  }, []);
+
+  //this will run whenever items or selectedCategory is changed. It essentually just filters by category right now
+  useEffect(() => {
+    if (selectedCategory === null) {
+      setFilteredItems(items);
+    } else {
+      const filtered = items.filter(
+        (item) => Number(item.category) === Number(selectedCategory)
+      );
+      setFilteredItems(filtered);
+    }
+  }, [items, selectedCategory]);
+
+  function processCategoryData(data: ItemType[]): CategoryType[] {
+    const categoryMap: { [key: number]: CategoryType } = {}; // Use a map to avoid duplicates
+
+    data.forEach((item) => {
+      const categoryId = item.category;
+      const categoryName = item.category_name;
+
+      if (!categoryMap[categoryId]) {
+        categoryMap[categoryId] = { id: categoryId, name: categoryName };
+      }
+    });
+
+    return Object.values(categoryMap);
+  }
+
+  const getUserItems = async () => {
+    try {
+      const cleanToken = authToken?.trim();
+      const URL = `http://127.0.0.1:8000/api/items/my_items/`; // get users posted items
+      const response = await axios.get(URL, {
+        headers: {
+          Authorization: `Bearer ${cleanToken}`,
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+
+      if (response.data && response.data.length > 0) {
+        setItems(response.data);
+        console.log("\n\nUser Items:", response.data);
+        setCategories(processCategoryData(response.data));
+        console.log("\n\nCategories:", categories);
+      } else {
+        console.error("No user data found in the response.");
+        Alert.alert("Error", "No user items found.");
+      }
+    } catch (error) {
+      console.error("Error getting user items:", error);
+      Alert.alert("Error", "Failed to get user items. Please try again.");
+    }
+  };
+
+  const handleCategorySelect = (categoryId: number | null) => {
+    setSelectedCategory(categoryId);
+    if (categoryId === null) {
+      clearCategories();
+      console.log("\n\nSelected category:", selectedCategory);
+    } else {
+      toggleCategory(categoryId);
+    }
+  };
   return (
-    <View>
-      <Text>My Items Screen</Text>
-    </View>
+    <>
+      <Stack.Screen
+        options={{
+          headerShown: true,
+          header: () => <Header />,
+        }}
+      />
+      <Categories
+        categories={categories}
+        selectedCategory={selectedCategory}
+        onSelectCategory={handleCategorySelect}
+      />
+      <ProductList
+        items={itemsToDisplay}
+        isLoading={isLoading}
+        // onFavoriteToggle={handleFavoriteToggle}
+      />
+    </>
   );
 };
 
